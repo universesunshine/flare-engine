cb282d638d7f420ecac1055a5518b1977608f873
Igor Paliychuk <mansonigor@gmail.com>
2014-10-16 15:39:03 +0300

    display something...

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -9,6 +9,6 @@ varying vec2 texcoord;
 
 void main()
 {
-    gl_Position = vec4(offsetX, offsetY, 0.0, 1.0);
+    gl_Position = vec4(position.x + offsetX, position.y + offsetY, 0.0, 1.0);
     texcoord = position * vec2(0.5) + vec2(0.5);
 }
